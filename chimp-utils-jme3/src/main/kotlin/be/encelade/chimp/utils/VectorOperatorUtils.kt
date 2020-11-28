8b1ce48293fd29d225b37b1a1a8961634a51f9a7
benckx <benoit.vleminckx@restore.eu>
2020-11-28 16:18:38 +0100

    Vector operators

--- chimp-utils-jme3/src/main/kotlin/be/encelade/chimp/utils/VectorOperatorUtils.kt
+++ chimp-utils-jme3/src/main/kotlin/be/encelade/chimp/utils/VectorOperatorUtils.kt
@@ -6,24 +6,31 @@ import com.jme3.math.Vector4f
 
 object VectorOperatorUtils {
 
+    // plus
     operator fun Vector2f.plus(other: Vector2f): Vector2f = this.add(other)
-
     operator fun Vector3f.plus(other: Vector3f): Vector3f = this.add(other)
-
     operator fun Vector4f.plus(other: Vector4f): Vector4f = this.add(other)
 
-    operator fun Vector3f.minus(other: Vector3f): Vector3f = this.minus(other)
-
+    // plus other types
     operator fun Vector3f.plus(other: Vector2f): Vector3f = this.add(Vector3f(other.x, other.y, 0f))
 
+    // minus
+    operator fun Vector2f.minus(other: Vector2f): Vector2f = this.minus(other)
+    operator fun Vector3f.minus(other: Vector3f): Vector3f = this.minus(other)
+    operator fun Vector4f.minus(other: Vector4f): Vector4f = this.minus(other)
+
+    // multiplication
     operator fun Vector3f.times(other: Vector3f): Vector3f = this.mult(other)
-
-    operator fun Vector2f.times(scalar: Float): Vector2f = this.mult(scalar)
-
-    operator fun Vector3f.times(scalar: Float): Vector3f = this.mult(scalar)
-
     operator fun Vector4f.times(other: Vector4f): Vector4f = this.mult(other)
 
+    // multiplication scalar
+    operator fun Vector2f.times(scalar: Float): Vector2f = this.mult(scalar)
+    operator fun Vector3f.times(scalar: Float): Vector3f = this.mult(scalar)
     operator fun Vector4f.times(scalar: Float): Vector4f = this.mult(scalar)
+
+    // divide scalar
+    operator fun Vector2f.div(scalar: Float): Vector2f = this.divide(scalar)
+    operator fun Vector3f.div(scalar: Float): Vector3f = this.divide(scalar)
+    operator fun Vector4f.div(scalar: Float): Vector4f = this.divide(scalar)
 
 }
